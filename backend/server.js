7592e7f84cbb9bbda8de690f9b57db997ad4a05d
Emanuel Felipe <emanuelfelipeperira2009@gmail.com>
2026-04-16 22:26:31 -0300

    corrigindo porta railway

--- backend/server.js
+++ backend/server.js
@@ -210,6 +210,8 @@ res.sendFile(path.join(__dirname, "../frontend/index.html"))
 
 // ================= SERVIDOR =================
 
-app.listen(3000, "0.0.0.0", () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, "0.0.0.0", () => {
 console.log("Servidor seguro rodando 🔐")
 })
